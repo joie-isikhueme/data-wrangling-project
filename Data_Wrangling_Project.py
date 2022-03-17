d63bf90c8f6f9fa3677f96f1ee35c21ecceff914
Joy <joie.isikhueme@gmail.com>
2022-03-17 11:49:47 +0100

    First Commit

---
+++ Data_Wrangling_Project.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import matplotlib.pyplot as plt
+
+wikiurl = "https://en.wikipedia.org/wiki/Road_safety_in_Europe"
+response= requests.get(wikiurl)
+print(response.status_code)
+soup= BeautifulSoup(response.text,'html.parser')
+wikiTable=soup.find('table',{'class':"wikitable sortable"})
+
+df=pd.read_html(str(wikiTable))
+df=pd.DataFrame(df[0])
+df.columns=[
+            'Country','Area', 'Population', 'GDP per Capita','Population Density', 
+            'Vehicle Ownership', 'Road Network Length', 'Total Road Deaths', 
+            'Road deaths per Million', 'Number of People Killed per Billion', 
+            'Number of Seriously Injured'
+            ]
+df.drop(['Number of People Killed per Billion',
+        'Number of Seriously Injured'],axis=1, 
+        inplace=True)
+
+df['Year']="2018"
+
+df['GDP per Capita']=df['GDP per Capita'].str.replace(r'[^\d]+','')
+df['Road Network Length']=df['Road Network Length'].str.replace(r'[^\d]+','')
+
+
+df= df.sort_values("Road deaths per Million")
+df.to_csv('Cleaned_European_Union_Safety_Facts_and_Figures.csv')
+
+highestdeath=df.nlargest(10,['Total Road Deaths'])
+mylabels= highestdeath['Country']
+myexplode =[0.2,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+highestdeath.groupby(['Country']).sum().plot(kind = 'pie',y= 'Total Road Deaths',
+                                            autopct = '%1.0f%%',labels=mylabels,
+                                            startangle=90,explode=myexplode,
+                                            radius = 1.2,frame=False,
+                                            shadow=True)
+
+
+# highestdeath.plot(kind = 'pie',y= 'Total Road Deaths',autopct = '%1.0f%%',labels=mylabels,startangle=90,explode=myexplode,)
+plt.legend(title= "Countries:",loc=2, prop={'size':6,},bbox_to_anchor =(-0.38,0.6,0,0.5),borderpad=2)
+plt.ylabel("")
+plt.title("Top 10 Countries With Highest Road Deaths(2018)",x=0.4,y=1.1)
+plt.show()
+mylabels
+highestdeath
+plt.scatter('Total Road Deaths','Population in thousands',data=highestdeath)
---
+++ README.md
@@ -0,0 +1,29 @@
+# **About The Project**
+
+This is a data wrangling project with the sole aim of utilizing the data from the wikipedia on European Union Safety Facts and Figures table. The table will be scraped ,cleaned and formatted for easier analysis.
+
+## Built With
+* Python 3.9
+* Beautiful Soup
+* Pandas
+* Matplotlib
+
+# **Getting Started**
+## Prerequisites
+* Python
+(https://www.python.org/downloads/)
+
+## Installation
+1. Clone the repo
+
+    `git clone`
+2. Install packages
+
+    `pip install -r requirements.txt`
+
+## Usage
+1. To run the file
+ 
+   `python {insert path of cloned file}`
+
+
